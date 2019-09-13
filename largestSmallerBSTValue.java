9e3e9e92f888a4b25541ac3b404817fdda91fe2b
coder1216 <yufeihon@usc.edu>
2019-09-13 16:51:40 -0700

    Update largestSmallerBSTValue.java
    week 1

--- largestSmallerBSTValue.java
+++ largestSmallerBSTValue.java
@@ -10,7 +10,7 @@ if cur.val >= target, root = root.left
 case2
 if cur.val < target, update global_closet if needed, root = root.right
 
-找到小于target的最大值
+***** 找到小于target的最大值 *****
 */
 
 class Solution{
@@ -22,6 +22,7 @@ class Solution{
             if(root.val >= target){
                 root = root.left;
             }else{
+                // 在小于target的情况下找最大的, if 当前root.val大于，就赋值给closet
                 if(closet < root.val){
                     closet = root.val;
                 }
@@ -32,9 +33,26 @@ class Solution{
     }
 }
 
+
 /*
- 
-
-
+ class Solution{
+    public int smallerBSTvalue(TreeNode root, int key){
+        if(root == null) throw new IllegalArgumentException();
+        int closet = root.val;
+        while(root != null){
+            if(key <= root.val){
+                root = root.left;
+            }else{
+                // key 大于root.val，找大于里面的最小的
+                if(closet < root.val){
+                    closet = root.val;
+                }
+                root = root.right;
+            }
+        }
+        return closet;
+    }
+}
 
 */
+
